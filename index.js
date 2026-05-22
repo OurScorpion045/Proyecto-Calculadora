f9a1de9cfdae282563ecb2d766df9c97aed4bf0b
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-22 11:50:20 -0700

    Avance: Archivo index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const visor = document.getElementById("visor");
+const del = document.getElementById("del")
+const num9 = document.getElementById("9");
+const num8 = document.getElementById("8");
 const num7 = document.getElementById("7");
 const num6 = document.getElementById("6");
 const num5 = document.getElementById("5");
@@ -13,6 +16,51 @@ const mult = document.getElementById("x");
 const div = document.getElementById("/");
 const result = document.getElementById("=");
 
+console.log(visor.innerHTML);
+
+del.addEventListener("click", () => {
+    let content = visor.innerText;
+    let long = content.length;
+    let result = content.substring(0, long - 1);
+    visor.innerHTML = result;
+})
+
+num9.addEventListener("click", () => {
+    visor.innerHTML += 9;
+})
+
+num8.addEventListener("click", () => {
+    visor.innerHTML += 8;
+})
+
 num7.addEventListener("click", () => {
     visor.innerHTML += 7;
+})
+
+num6.addEventListener("click", () => {
+    visor.innerHTML += 6;
+})
+
+num5.addEventListener("click", () => {
+    visor.innerHTML += 5;
+})
+
+num4.addEventListener("click", () => {
+    visor.innerHTML += 4;
+})
+
+num3.addEventListener("click", () => {
+    visor.innerHTML += 3;
+})
+
+num2.addEventListener("click", () => {
+    visor.innerHTML += 2;
+})
+
+num1.addEventListener("click", () => {
+    visor.innerHTML += 1;
+})
+
+num0.addEventListener("click", () => {
+    visor.innerHTML += 0;
 })
